ccfac1fd477678d2c3bd3383fc8d71344dda507c
dev-KarimAhmed <dev-KarimAhmed@gmail.com>
2023-07-19 13:44:28 +0300

    create listview.builder for note item

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,7 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
-import 'package:notes_app/widgets/custom_note_item.dart';
+import 'package:notes_app/widgets/notes_list.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({Key? key}) : super(key: key);
@@ -15,7 +15,7 @@ class NotesView extends StatelessWidget {
           child: Column(
             children: [
               CustomAppBar(),
-              CustomNoteItem()
+              Expanded(child: NotesList()),
             ],
           ),
         ),
---
+++ lib/widgets/notes_list.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/custom_note_item.dart';
+
+class NotesList extends StatelessWidget {
+  const NotesList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(itemBuilder: (context , index){
+      return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: CustomNoteItem(),
+      );
+    });
+  }
+}

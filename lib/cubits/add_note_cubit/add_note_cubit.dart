e19f454d9a54929da9e2cf0c6f697390c3464912
dev-KarimAhmed <dev-KarimAhmed@gmail.com>
2023-07-20 13:02:01 +0300

    create add note_state and add note_cubit

---
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_state.dart';
+
+
+class AddNotesCubit extends Cubit<AddNotesState>{
+  AddNotesCubit(super.initialState);
+
+
+}
---
+++ lib/cubits/add_note_cubit/add_note_state.dart
@@ -0,0 +1,3 @@
+abstract class AddNotesState{}
+
+class AddNotesInitial extends AddNotesState{}

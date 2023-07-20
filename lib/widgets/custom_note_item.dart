15237ff83416c4a20811e370ee23316527870a4f
dev-KarimAhmed <dev-KarimAhmed@gmail.com>
2023-07-20 06:28:39 +0300

    create EditNoteView

--- lib/widgets/custom_note_item.dart
+++ lib/widgets/custom_note_item.dart
@@ -1,37 +1,46 @@
 import 'package:flutter/material.dart';
 
+import '../views/edit_note_view.dart';
+
 class CustomNoteItem extends StatelessWidget {
   const CustomNoteItem({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.fromLTRB(16, 24, 0, 25),
-      decoration: BoxDecoration(
-        color: Color(0xffFFCE7F),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Text('Flutter Tips', style: TextStyle(color: Colors.black),),
-            subtitle: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16),
-              child: Text('Build your career with Tharwat samy' , style: TextStyle(color: Colors.black45),),
+    return GestureDetector(
+      onTap: (){
+        Navigator.push(context , MaterialPageRoute(builder: (context){
+          return EditNoteView();
+        }));
+      },
+      child: Container(
+        padding: EdgeInsets.fromLTRB(16, 24, 0, 25),
+        decoration: BoxDecoration(
+          color: Color(0xffFFCE7F),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: Text('Flutter Tips', style: TextStyle(color: Colors.black),),
+              subtitle: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                child: Text('Build your career with Tharwat samy' , style: TextStyle(color: Colors.black45),),
+              ),
+              trailing: IconButton(
+                onPressed: (){},
+                icon: Icon(Icons.delete , size: 30,),
+                color: Colors.black,
+              ),
+              textColor: Colors.black,
             ),
-            trailing: IconButton(
-              onPressed: (){},
-              icon: Icon(Icons.delete , size: 30,),
-              color: Colors.black,
+            Padding(
+              padding: const EdgeInsets.only(right: 16.0),
+              child: Text('May 21,2023' , style: TextStyle(color: Colors.black45),),
             ),
-            textColor: Colors.black,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 16.0),
-            child: Text('May 21,2023' , style: TextStyle(color: Colors.black45),),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }

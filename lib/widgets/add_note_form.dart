d36a900aa7c6839f9b7f248af4bd1a04daf29e95
dev-KarimAhmed <dev-KarimAhmed@gmail.com>
2023-07-23 08:55:47 +0300

    format date

--- lib/widgets/add_note_form.dart
+++ lib/widgets/add_note_form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 
 import '../cubits/add_note_cubit/add_note_cubit.dart';
 import '../cubits/add_note_cubit/add_note_state.dart';
@@ -45,7 +46,7 @@ class _AddNoteFormState extends State<AddNoteForm> {
                   var note = NoteModel(
                       title: title! ,
                       subTitle: subTitle! ,
-                      date: DateTime.now().toString(),
+                      date: DateFormat.yMEd().add_jm().format(DateTime.now()),
                       color: Colors.blue.value
                   );
                   BlocProvider.of<AddNotesCubit>(context).addNote(note: note);
